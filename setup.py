4f67a1db4625ab328004fd43f574e00c4d244bd9
Paul Harper <benekastah@gmail.com>
2021-09-29 10:53:54 -0700

    v1.2.14

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-helpshift",
-    version="1.2.13",
+    version="1.2.14",
     description="Singer.io tap for extracting data",
     author="Stitch",
     url="http://singer.io",
