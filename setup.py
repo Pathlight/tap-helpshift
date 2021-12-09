71b16c00cc75e1fb2d50f7a0f78f34007846e9e9
Paul Harper <benekastah@gmail.com>
2021-12-09 13:56:16 -0800

    bump to v1.2.27

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-helpshift",
-    version="1.2.26",
+    version="1.2.27",
     description="Singer.io tap for extracting data",
     author="Stitch",
     url="http://singer.io",
